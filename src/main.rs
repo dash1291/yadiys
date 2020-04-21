63cb7a50574b4555d17a86da2e8c3387feb3a67c
Ashish Dubey <ashish.dubey91@gmail.com>
2020-04-22 01:16:33 +0530

    rise

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+pub mod oscillator;
+use crate::oscillator::Oscillator;
+
+extern crate portaudio;
+
+use portaudio as pa;
+use std::f64::consts::PI;
+
+const CHANNELS: i32 = 1;
+const NUM_SECONDS: i32 = 5;
+const SAMPLE_RATE: f64 = 44_100.0;
+const FRAMES_PER_BUFFER: u32 = 64;
+const TABLE_SIZE: usize = 200;
+
+fn main() {
+    match run() {
+        Ok(_) => {}
+        e => {
+            eprintln!("Example failed with the following: {:?}", e);
+        }
+    }
+}
+
+fn run() -> Result<(), pa::Error> {
+    println!(
+        "PortAudio Test: output sine wave. SR = {}, BufSize = {}",
+        SAMPLE_RATE, FRAMES_PER_BUFFER
+    );
+
+    let pa = pa::PortAudio::new()?;
+
+    let mut phase = 0.;
+    let frequency = 440.;
+    let mut settings =
+        pa.default_output_stream_settings(CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER)?;
+    // we won't output out of range samples so don't bother clipping them.
+    settings.flags = pa::stream_flags::CLIP_OFF;
+
+    // This routine will be called by the PortAudio engine when audio is needed. It may called at
+    // interrupt level on some machines so don't do anything that could mess up the system like
+    // dynamic resource allocation or IO.
+    let mut osc = Oscillator::new(880.);
+    
+    let callback = move |pa::OutputStreamCallbackArgs { buffer, frames, .. }| {
+        osc.output(buffer, frames);
+
+        pa::Continue
+    };
+
+    let mut stream = pa.open_non_blocking_stream(settings, callback)?;
+
+    stream.start()?;
+
+    println!("Play for {} seconds.", NUM_SECONDS);
+    pa.sleep(NUM_SECONDS * 1_000);
+
+    stream.stop()?;
+    stream.close()?;
+
+    println!("Test finished.");
+
+    Ok(())
+}
---
+++ src/oscillator.rs
@@ -0,0 +1,17 @@
+pub struct Oscillator {
+    frequency: f32,
+    current_phase: f32,
+}
+
+impl Oscillator {
+    pub fn new(frequency: f32) -> Oscillator {
+        return Oscillator { frequency, current_phase: 0. }
+    }
+
+    pub fn output(&mut self, outbuf: &mut [f32], size: usize) {
+        for i in 0..size {
+            self.current_phase = (self.current_phase + self.frequency / 44100.) % 1.;
+            outbuf[i] = f32::sin(2. * 3.14159265 * self.current_phase);
+        }
+    }
+}
